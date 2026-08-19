e60d7c2766a7bfe91a7324ab8e0f6d76daff91aa
TheBizzle <jason.bertsche@gmail.com>
2026-08-19 11:44:30 -0500

    Delete: Dead, fraudulent code

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,7 @@ use std::fs::read_to_string;
 use std::io::{Write, stdin, stdout};
 use std::process::exit;
 
-use rox::chunk::Chunk;
-
-use rox::compiler::compile;
-
-use rox::disassembler::disassemble_chunk;
-
-use rox::opcode::OpCode::{Add, Constant, Divide, Negate, Return};
-
-use rox::value::Value::Double;
-
-use rox::vm::Interpretation::{self, CompilationError, RuntimeError, Success};
+use rox::vm::Interpretation::{CompilationError, RuntimeError, Success};
 use rox::vm::VM;
 
 #[tokio::main]
@@ -73,36 +63,4 @@ fn run_file(vm: &VM, filepath: &str) {
 fn interpret(_vm: &VM, source: &str) -> Interpretation {
   compile(source);
   Success
-}
-
-fn _fraudulent_main() {
-  let mut vm = VM::init();
-
-  let mut chunk = Chunk::default();
-
-  let constant_index = chunk.add_constant(Double(1.2));
-  chunk.write(Constant as u8, 123);
-  chunk.write(constant_index, 123);
-
-  let constant_index2 = chunk.add_constant(Double(1.8));
-  chunk.write(Constant as u8, 123);
-  chunk.write(constant_index2, 123);
-
-  chunk.write(Add as u8, 123);
-
-  let constant_index3 = chunk.add_constant(Double(3.0));
-  chunk.write(Constant as u8, 123);
-  chunk.write(constant_index3, 123);
-
-  chunk.write(Divide as u8, 123);
-  chunk.write(Negate as u8, 123);
-
-  chunk.write(Return as u8, 123);
-
-  disassemble_chunk(&chunk, "test chunk");
-
-  vm.interpret(&chunk);
-
-  let _ = vm.free();
-  let _ = chunk.free();
 }
