b601d05723e577c923faca63203dc54a61024d4e
TheBizzle <jason.bertsche@gmail.com>
2026-09-05 19:09:25 -0500

    Optimization: Cut GC frequency by an order of magnitude

--- src/vm.rs
+++ src/vm.rs
@@ -596,7 +596,7 @@ impl VM {
 
       if DEBUG_STRESS_GC {
         self.collect_garbage();
-      } else if self.instrs_since_last_gc >= 999 {
+      } else if self.instrs_since_last_gc >= 10000 {
         self.collect_garbage();
         self.instrs_since_last_gc = 0;
       } else {
