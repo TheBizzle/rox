6815445459c6ccee1725738c5bb307e532c36dfd
TheBizzle <jason.bertsche@gmail.com>
2026-09-19 10:54:54 -0500

    Test: Improve CLI error reporting for Wasm

--- wasm-backend/src/lib.rs
+++ wasm-backend/src/lib.rs
@@ -14,9 +14,30 @@ use crate::wasm_result::WasmResult::{self, CompilationError, Success, Validation
 pub fn generate_wasm(source: &str) -> WasmResult {
   if let Some((compilation, _)) = Compiler::default().run(source.to_string()) {
     let wasm = WasmCompiler::new().run(&compilation);
+
     match validate(&wasm) {
       Ok(_) => Success { wasm },
-      Err(err) => ValidationError { message: err.to_string() },
+
+      Err(err) => {
+        let mut config = wasmprinter::Config::new();
+        config.print_offsets(true);
+
+        let mut storage = String::new();
+        let printed = match config.offsets_and_lines(&wasm, &mut storage) {
+          Ok(offset_line_pairs) => offset_line_pairs
+            .map(|(offset_opt, line)| {
+              offset_opt.map_or_else(
+                || format!("          {line}"),
+                |offset_number| format!("{offset_number:#06x}  {line}"),
+              )
+            })
+            .collect(),
+
+          Err(print_error) => format!("Could not print Wasm: {print_error}"),
+        };
+
+        ValidationError { message: printed + "\n\n" + &err.to_string() }
+      },
     }
   } else {
     CompilationError
