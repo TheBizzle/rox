f0e40f42e0940e73b53759250d77fc0bf6d9daf3
TheBizzle <jason.bertsche@gmail.com>
2026-09-20 09:14:56 -0500

    Prefactor: Prepare `ShadowStack` for dynamically-typed values

--- wasm-backend/src/shadow_stack.rs
+++ wasm-backend/src/shadow_stack.rs
@@ -1,7 +1,7 @@
 #![allow(unused)]
 
 #[derive(Eq, PartialEq)]
-enum LoxType {
+enum StackValueType {
   Boolean,
   BoundMethod,
   Class,
@@ -13,10 +13,12 @@ enum LoxType {
   Raw,
   String,
   Upvalue,
+  TypeTag,
+  Any,
 }
 
 pub struct ShadowStack {
-  stack: Vec<LoxType>,
+  stack: Vec<StackValueType>,
 }
 
 impl ShadowStack {
@@ -28,20 +30,24 @@ impl ShadowStack {
     self.stack.is_empty()
   }
 
-  fn peek(&self, n: usize) -> Option<&LoxType> {
+  fn peek(&self, n: usize) -> Option<&StackValueType> {
     self.stack.get(self.stack.len() - 1 - n)
   }
 
+  pub fn peek_any(&self, n: usize) -> bool {
+    self.peek(n) == Some(&StackValueType::TypeTag)
+  }
+
   pub fn peek_boolean(&self, n: usize) -> bool {
-    self.peek(n) == Some(&LoxType::Boolean)
+    self.peek(n) == Some(&StackValueType::Boolean)
   }
 
   pub fn peek_nil(&self, n: usize) -> bool {
-    self.peek(n) == Some(&LoxType::Nil)
+    self.peek(n) == Some(&StackValueType::Nil)
   }
 
   pub fn peek_number(&self, n: usize) -> bool {
-    self.peek(n) == Some(&LoxType::Number)
+    self.peek(n) == Some(&StackValueType::Number)
   }
 
   pub fn peek_reference(&self, n: usize) -> bool {
@@ -57,47 +63,51 @@ impl ShadowStack {
     self.stack.pop();
   }
 
+  pub fn push_any(&mut self) {
+    self.stack.push(StackValueType::TypeTag);
+  }
+
   pub fn push_boolean(&mut self) {
-    self.stack.push(LoxType::Boolean);
+    self.stack.push(StackValueType::Boolean);
   }
 
   pub fn push_bound_method(&mut self) {
-    self.stack.push(LoxType::BoundMethod);
+    self.stack.push(StackValueType::BoundMethod);
   }
 
   pub fn push_class(&mut self) {
-    self.stack.push(LoxType::Class);
+    self.stack.push(StackValueType::Class);
   }
 
   pub fn push_closure(&mut self) {
-    self.stack.push(LoxType::Closure);
+    self.stack.push(StackValueType::Closure);
   }
 
   pub fn push_native_fn(&mut self) {
-    self.stack.push(LoxType::NativeFn);
+    self.stack.push(StackValueType::NativeFn);
   }
 
   pub fn push_nil(&mut self) {
-    self.stack.push(LoxType::Nil);
+    self.stack.push(StackValueType::Nil);
   }
 
   pub fn push_number(&mut self) {
-    self.stack.push(LoxType::Number);
+    self.stack.push(StackValueType::Number);
   }
 
   pub fn push_object(&mut self) {
-    self.stack.push(LoxType::Object);
+    self.stack.push(StackValueType::Object);
   }
 
   pub fn push_raw(&mut self) {
-    self.stack.push(LoxType::Raw);
+    self.stack.push(StackValueType::Raw);
   }
 
   pub fn push_string(&mut self) {
-    self.stack.push(LoxType::String);
+    self.stack.push(StackValueType::String);
   }
 
   pub fn push_upvalue(&mut self) {
-    self.stack.push(LoxType::Upvalue);
+    self.stack.push(StackValueType::Upvalue);
   }
 }
